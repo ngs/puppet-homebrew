a160ce7809c0dddc9f54ccd2381f124e382257a5
Atsushi Nagase <a@ngs.io>
2014-04-27 04:19:29 +0900

    boxen-brews -> homebrew-brews

--- manifests/formula.pp
+++ manifests/formula.pp
@@ -16,26 +16,19 @@ define homebrew::formula($source = undef) {
     'group'  => 'staff'
   })
 
-  ensure_resource('file', "${homebrew::tapsdir}/boxen/boxen-brews", {
+  ensure_resource('file', "${homebrew::tapsdir}/boxen/homebrew-brews", {
     'ensure' => 'directory',
     'owner'  => $::boxen_user,
     'group'  => 'staff'
   })
 
-  ensure_resource('file', "${homebrew::tapsdir}/boxen/boxen-brews/.git", {
+  ensure_resource('file', "${homebrew::tapsdir}/boxen/homebrew-brews/.git", {
     'ensure' => 'directory',
     'owner'  => $::boxen_user,
     'group'  => 'staff'
   })
 
-  ensure_resource('file', "${homebrew::config::formuladir}/boxen/brews", {
-    'ensure' => 'link',
-    'owner'  => $::boxen_user,
-    'group'  => 'staff',
-    'target' => "${homebrew::config::tapsdir}/boxen/boxen-brews",
-  })
-
-  file { "${homebrew::config::tapsdir}/boxen/boxen-brews/${name}.rb":
+  file { "${homebrew::config::tapsdir}/boxen/homebrew-brews/${name}.rb":
     source  => $formula_source
   }
 
